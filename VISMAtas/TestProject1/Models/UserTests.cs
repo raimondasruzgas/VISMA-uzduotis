915c0f6ef05ed519c0e9aaeee0903fd8056a4dc1
Aiwiux <89936697+raimondasruzgas@users.noreply.github.com>
2024-03-27 08:58:31 +0200

    Update files
    Visma task

---
+++ VISMAtas/TestProject1/Models/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VISMAtas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VISMAtas.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            User user = new User();
+            user.Name = "Raimis";
+            Assert.AreEqual("Raimis", user.ToString());
+        }
+    }
+}
---
+++ VISMAtas/TestProject1/UI_HelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VISMAtas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VISMAtas.Tests
+{
+    [TestClass()]
+    public class UI_HelperTests
+    {
+        [TestMethod()]
+        public void AskForStringTest()
+        {
+           Assert.IsTrue(true);
+        }
+    }
+}
